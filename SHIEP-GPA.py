a0f35c9720dd437c8ae20df56759ce455fca320a
surpassing <shinejits@gmail.com>
2014-06-21 19:39:08 +0800

    Update SHIEP-GPA.py
    不用手动筛选免听重修的科目了，只要复制即可，但是挂科的情况依旧没有测试，估计以后不太会再修改了。

--- SHIEP-GPA.py
+++ SHIEP-GPA.py
@@ -1,26 +1,27 @@
 # encoding = utf-8
 #
-# SHIEP 总学分，算数平均分,绩点等快速计算。
-# Sam 2014-6-20 毕业后第一天写此脚本以用来算出自己的最好看绩点形式 :D
+# SHIEP 总学分，总绩点，算数平均分，平均绩点GPA等快速计算。
+# Sam 2014-6-20所写 毕业后第一天写此脚本以用来算出自己的最好看绩点形式 :D
 #
-# 挂过科的情况本人未测试,0学分科目不在计算范围内。
-# SHIEP-GPA-CALCULATOR version 1.5 with python 3.3
-# 使用方法:复制成绩页面所有内容到本地文档，有重修科目选择取得最高分的一项，低的那一行全部删除，与本程序放置到同一个文件夹中,运行py。
+# 挂过科的情况本人未测试(去掉不规则的行数试试)，免听重修的可以使用本脚本进行计算，0学分科目不在计算范围内。
+# SHIEP-GPA-CALCULATOR version 2.0 beta with python 3.3 6-21-2014
+# 使用方法:复制成绩页面所有表格内内容到本地文档，与本程序放置到同一个文件夹中,运行py。
 
-type_in = input("请输入文件名(请注意有无后缀,建议使用纯文件不使用txt因为未测试过后者!): ")
+type_in = input("请输入文件名(请注意有无后缀): ")
 doc_path = './' + type_in
 
 with open(doc_path,'r',encoding='utf-8') as data:
     text = data.readlines()
 
-def calc():
-    course_sum = 0   # 非0学分课程总数
-    grade_sum = 0    # 总分数
-    credit_sum = 0   # 总学分
-    total_ctp = 0
+course_in_all = {}
+
+def make_dict():
+    global course_in_all
 
     for each_column in text:
         value = each_column.split()
+#       take_it_over = value[9]   # 是否重修
+        course_num = value[1]     # 课号
         course_credit = value[4]  # 学分
         course_grade = value[5]   # 分数
         course_point = value[6]   # 绩点
@@ -35,23 +36,45 @@ def calc():
             elif course_grade == '及格' or course_grade == 'D':
                 course_grade = '60'
 
-            grade_sum += int(course_grade)  #总分数
-            course_sum += 1  #总科目数
-            credit_sum += float(course_credit)  #总学分
+            if course_num not in course_in_all:
+                course_in_all[course_num] = [course_credit, course_grade, course_point]
+            else:
+                  if course_grade >= course_in_all[course_num][1]:
+                      course_in_all[course_num][1] = course_grade
+                      course_in_all[course_num][2] = course_point
 
-            #gpa计算用
-            credit_times_point = float(course_credit) * float(course_point)  #单项学分乘以绩点
-            total_ctp += credit_times_point  #学分乘以绩点的总数
+def calc():
+    global course_in_all
+    course_sum, credit_sum, grade_sum, point_sum = 0, 0, 0, 0
+    total_ctp = 0
 
-    my_gpa = total_ctp / credit_sum  #  学分乘以绩点的总数除以总学分
-    aver_sum = grade_sum / course_sum
+    for i in course_in_all:
+        course_sum += 1
+        credit_sum += float(course_in_all[i][0])  # 学分
+        grade_sum += float(course_in_all[i][1])   # 分数
+        point_sum += float(course_in_all[i][2])   # 绩点
 
-    print("显示【学分不等于0】的所有科目(包括换算后的优良中差,ABCD)")
+        # gpa计算用
+        credit_times_point = float(course_in_all[i][0]) * float(course_in_all[i][2])  # 单项学分乘以绩点
+        total_ctp += credit_times_point  # 学分乘以绩点的总数
+
+       # 平均分计算用
+        aver_point = grade_sum / course_sum
+        if  aver_point >= 90:
+            aver_point_final = 4
+        else:
+            aver_point_final = (aver_point - 50) / 10
+
+    my_gpa = total_ctp / credit_sum  # 学分乘以绩点的总数除以总学分
+
+    print(course_sum, credit_sum, grade_sum, point_sum)
+    print("上海电力外语系2010级某只求高GPA的路过，本脚本显示【学分不等于0】的所有科目(包括换算后的优良中差,ABCD)")
+    print("鸣谢:所有给我高分的那些老师们！有机会的话在3.0里加入教师统计给你们统统赞一个，我都记得的。")
     print('总学分是：', credit_sum)
-    print('课程总数是：', course_sum)
+    print('总课程数是：', course_sum)
     print('总分数是：', grade_sum)
     print('GPA是：', round(my_gpa, 5))
-    print('算数平均分是：', round(aver_sum, 3))
+    print('算数平均分是：', round(aver_point, 3), ', 即', round(aver_point_final, 5))
 
+make_dict()
 calc()
-print()
